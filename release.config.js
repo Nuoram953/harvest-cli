d41bf9b2b2e3f63da3c5552dc894fe04d86f58d7
Nuoram953 <antoinemaroun@gmail.com>
2025-08-02 18:38:07 -0400

    feat: initial beta release

--- release.config.js
+++ release.config.js
@@ -11,7 +11,7 @@ module.exports = {
       "@semantic-release/git",
       {
         assets: ["CHANGELOG.md", "package.json", "package-lock.json"],
-        message: "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+        message: "chore: ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
       },
     ],
   ],
